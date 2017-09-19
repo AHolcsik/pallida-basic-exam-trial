c6168dbd1c959dc3da6e16eea9083e934b2935da
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-19 10:03:14 +0200

    feat(trial): name_from_email.py exercise done, tested

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -6,22 +6,14 @@
 # accents does not matter
 
 import re
-email = "elek.viz@exam.com"
-#email = str(input("Give me an email address (e.g. elek.viz@exam.com) "))
+email = str(input("Give me an email address (e.g. elek.viz@exam.com) "))
 
 def name_from_email(email):
     first_name = ""
     last_name = ""
     match = re.search('([\w.-]+)\.([\w.-]+)', email)
     if match:
-        print(match.group(1))
-        print(match.group(2))
+        print((match.group(2)), (match.group(1)))
 
-    # for element in email != "\.":
-    #     first_name += element
-    #     for character in email != "@":
-    #         last_name += character
-    # return(last_name, first_name)
-
-print(name_from_email("elek.viz@exam.com"))
+name_from_email(email)
 
