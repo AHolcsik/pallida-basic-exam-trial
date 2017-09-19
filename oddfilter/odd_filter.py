d2f74cca663f85f139757aad1646f8c0cee338fe
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-19 10:10:55 +0200

    feat(trial): structure set in odd_filter.py exercise

--- oddfilter/odd_filter.py
+++ oddfilter/odd_filter.py
@@ -2,8 +2,13 @@
 # and returns a new list with every odd number from the orignal list
 # example: [1, 2, 3, 4, 5] should produce [1, 3, 5]
 
-def find_odd_numbers(*arg):
-    
+list_o_numbers = [1, 2, 3, 4, 5]
+def odd_filter(list_o_numbers):
+    odds = []
+    for element in list_o_numbers:
+        if element % 2 != 0:
+            odds += element
+    return(odds)
 
 
-print(odd_filter([1, 2, 3, 4, 5]))
+print(odd_filter(list_o_numbers))
