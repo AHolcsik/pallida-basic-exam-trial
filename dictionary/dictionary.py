e3a4a086d7050f8cc7e18ea7b990ecac47d19339
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-19 12:39:47 +0200

    feat(trial): dictionary.py exercise continued

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -6,10 +6,10 @@ dictionary = [
 # Implement this method. It should add the given key-value pair to the
 # list 'dictionary'
 
-dictionary["kutya"] = "dog"
+#dictionary["kutya"] = "dog"
 
-# hun_word = "kutya"
-# eng_word = "dog"
+hun_word = "alma"
+#eng_word = "apple"
 
 # def add_word(hun_word, eng_word):
 #     dictionary[hun_word] = eng_word
@@ -18,12 +18,22 @@ dictionary["kutya"] = "dog"
 # word form the list 'dictionary'
 
 
-def translate_to_hun(eng_word):
-    pass
+# def translate_to_hun(eng_word):
+#     hungarian = ""
+#     for p in dictionary:
+#         if p["apple"] == eng_word:
+#             print(p)
+
+
+def search("apple"):
+    for p in dictionary:
+        if p["alma"] == "apple":
+            return p
+
+print(search("apple"))    
 
 
 def translate_to_eng(hun_word):
     pass
 
 # add_word(hun_word, eng_word)
-print(dictionary)
