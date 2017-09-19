c210c9320bf4628c48c98ebd7e21a4f65cf7e768
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-19 10:05:03 +0200

    feat(trial): odd_filter.py exercise started

--- oddfilter/odd_filter.py
+++ oddfilter/odd_filter.py
@@ -2,4 +2,8 @@
 # and returns a new list with every odd number from the orignal list
 # example: [1, 2, 3, 4, 5] should produce [1, 3, 5]
 
-print(odd_filter([1, 2, 3, 4, 5]))  # should print [1, 3, 5]
+def find_odd_numbers(*arg):
+    
+
+
+print(odd_filter([1, 2, 3, 4, 5]))
