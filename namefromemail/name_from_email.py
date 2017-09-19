338df788265bd5d643583fd26221a68539eff52d
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-19 09:28:01 +0200

    feat(trial): name_from_email.py exercise continued, code built, testing

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -5,13 +5,17 @@
 # example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
 # accents does not matter
 
-email = str(input("Give me an email address (e.g. elek.viz@exam.com) "))
+email = "elek.viz@exam.com"
+#email = str(input("Give me an email address (e.g. elek.viz@exam.com) "))
 
 def name_from_email(email):
-    
-
-
-
+    first_name = ""
+    last_name = ""
+    if element in email != ".":
+        first_name += element
+        if character in email != "@":
+            last_name += character
+    return(last_name, first_name)
 
 print(name_from_email("elek.viz@exam.com"))
 
