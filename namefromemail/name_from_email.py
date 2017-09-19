a76896510889a180e7c5863d1c7d6961f23331ca
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-19 09:56:46 +0200

    fix(trial): structure rearranged in name_from_email.py exercise

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -5,17 +5,23 @@
 # example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
 # accents does not matter
 
+import re
 email = "elek.viz@exam.com"
 #email = str(input("Give me an email address (e.g. elek.viz@exam.com) "))
 
 def name_from_email(email):
     first_name = ""
     last_name = ""
-    if element in email != ".":
-        first_name += element
-        if character in email != "@":
-            last_name += character
-    return(last_name, first_name)
+    match = re.search('([\w.-]+)\.([\w.-]+)', email)
+    if match:
+        print(match.group(1))
+        print(match.group(2))
+
+    # for element in email != "\.":
+    #     first_name += element
+    #     for character in email != "@":
+    #         last_name += character
+    # return(last_name, first_name)
 
 print(name_from_email("elek.viz@exam.com"))
 
